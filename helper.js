09f128a0f7cb12735329029b8162e322c6118aa3
mehari <gmehari.edu@gmail.com>
2018-05-16 18:04:36 +0300

    :tshirt: chore(core): integrate eslint and prettier.

--- helper.js
+++ helper.js
@@ -1,65 +1,69 @@
-﻿const winston = require('winston');
-winston.add(winston.transports.File, { filename: 'backup.log' });
+﻿const winston = require("winston");
+
+winston.add(winston.transports.File, { filename: "backup.log" });
 winston.remove(winston.transports.Console);
 
-const { exec } = require('child_process');
-const path = require('path');
-const nodemailer = require('nodemailer');
+const { exec } = require("child_process");
+const path = require("path");
+const nodemailer = require("nodemailer");
 
 const backupDB = (db, basePath, cb) => {
-    winston.log('info', 'dumping db ...');
-    let currentDate = new Date().toGMTString();
-    let fileName = currentDate.replace(/[, .*+?^${}()|[\]\\]/g, '')// + '.gzip'
-    let filePath = path.join(basePath, fileName);
+  winston.log("info", "dumping db ...");
+  const currentDate = new Date().toGMTString();
+  const fileName = currentDate.replace(/[, .*+?^${}()|[\]\\]/g, ""); // + '.gzip'
+  const filePath = path.join(basePath, fileName);
 
-    exec('mongodump --db ' + db + ' --gzip --archive=' + filePath, function (err, stdout, stderr) {
-        return cb(null, fileName);
-    });
-}
+  exec(`mongodump --db ${db} --gzip --archive=${filePath}`, () =>
+    cb(null, fileName)
+  );
+};
 const emailSender = (data, cb) => {
-    winston.log('info', 'sending email ...');
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // secure:true for port 465, secure:false for port 587
-        auth: {
-            user: data.email,
-            pass: data.password
-        }
-    });
-
-    let attachments = [];
-
-    let filePath = path.join(data.path, data.fileName);
-    attachments[attachments.length] = { path: filePath };
-
-    let mailOptions = {
-        from: data.email,
-        to: data.email, // list of receivers
-        subject: data.subject, // Subject line
-        html: '',
-        attachments: attachments
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            winston.log('warn', error);
-            return cb(error);
-        }
-        return cb(null, info);
-    });
-}
-module.exports = {
-    backup: (db, basePath, email, password, project) => {
-        winston.log('info', 'sending email ...');
-        backupDB(db, basePath, function (err, fileName) {
-            emailSender({
-                email: email,
-                password: password,
-                path: basePath,
-                fileName: fileName,
-                subject: project + ' ' + fileName
-            }, function (err, info) { })
-        })
+  winston.log("info", "sending email ...");
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+      user: data.email,
+      pass: data.password
     }
-}
+  });
+
+  const attachments = [];
+
+  const filePath = path.join(data.path, data.fileName);
+  attachments[attachments.length] = { path: filePath };
+
+  const mailOptions = {
+    from: data.email,
+    to: data.email, // list of receivers
+    subject: data.subject, // Subject line
+    html: "",
+    attachments
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      winston.log("warn", error);
+      return cb(error);
+    }
+    return cb(null, info);
+  });
+};
+module.exports = {
+  backup: (db, basePath, email, password, project) => {
+    winston.log("info", "sending email ...");
+    backupDB(db, basePath, (err, fileName) => {
+      emailSender(
+        {
+          email,
+          password,
+          path: basePath,
+          fileName,
+          subject: `${project} ${fileName}`
+        },
+        () => {}
+      );
+    });
+  }
+};
